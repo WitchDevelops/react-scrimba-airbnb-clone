7db89ee6bad673890f5851439342b2513f855a6e
WitchDevelops <d.n.wojewska@gmail.com>
2023-10-26 19:49:29 +0200

    #7 resize images and convert to webp

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,28 +21,38 @@ type CardProps = {
 const Card: React.FC<CardProps> = (props) => {
   let badgeText;
   if (props.item.openSpots === 0) {
-    badgeText="SOLD OUT";
+    badgeText = "SOLD OUT";
   } else if (props.item.location === "Online") {
-    badgeText="ONLINE";
+    badgeText = "ONLINE";
   }
 
   return (
     <figure className="card__container">
-        <img className="card__image" src={`/src/assets/${props.item.coverImg}`} alt={props.item.alt}>
-        </img>
-        {badgeText && <p className="card__badge">{badgeText}</p>}
-        <figcaption className="card__info">
-            <div className="rating__container">
-                <img className="rating__icon" src={star} alt="star rating"></img>
-                <p className="rating__value">{props.item.stats.rating}</p>
-                <p className="rating__number">({props.item.stats.reviewCount})</p>&#x2022;
-                <p className="rating__location">{props.item.location}</p>
-            </div>
-            <p className="card__title">{props.item.title}</p>
-            <p className="card__price"><strong className="card__price--strong">From ${props.item.price}</strong>/ person</p>
-        </figcaption>
+      <img
+        className="card__image"
+        src={`/src/assets/${props.item.coverImg}`}
+        alt={props.item.alt}
+        loading="lazy"
+      ></img>
+      {badgeText && <p className="card__badge">{badgeText}</p>}
+      <figcaption className="card__info">
+        <div className="rating__container">
+          <img className="rating__icon" src={star} alt="star rating"></img>
+          <p className="rating__value">{props.item.stats.rating}</p>
+          <p className="rating__number">({props.item.stats.reviewCount})</p>
+          &#x2022;
+          <p className="rating__location">{props.item.location}</p>
+        </div>
+        <p className="card__title">{props.item.title}</p>
+        <p className="card__price">
+          <strong className="card__price--strong">
+            From ${props.item.price}
+          </strong>
+          / person
+        </p>
+      </figcaption>
     </figure>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
